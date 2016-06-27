6fab54570b9a338711624d210131adf3b34aa51a
John Whisker <johnwhisker@Johns-MacBook-Air.local>
2016-06-27 19:25:54 +0700

    Init

---
+++ Dating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dating
+//
+//  Created by John Whisker on 6/27/16.
+//  Copyright © 2016 John Whisker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
